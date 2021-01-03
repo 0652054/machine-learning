52b32658303cb2aa2bcae448bbf0cb101cd207f3
0652054 <73530138+0652054@users.noreply.github.com>
2021-01-04 02:33:11 +0800

    Add files via upload

---
+++ final/2P.py
@@ -0,0 +1,104 @@
+"""
+The template of the script for the machine learning process in game pingpong
+"""
+# ball_position_history[-1][-2] ball_x
+# ball_position_history[-1][-1] ball_y
+# scene_info["ball_speed"][-2]  ball_speed_x
+# scene_info["ball_speed"][-1]  ball_speed_y
+# scene_info["platform_2P"][-2] platform_center_x
+# scene_info["platform_2P"][-1] platform_center_y
+
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import math
+import random
+model = LinearRegression()
+ball_position_history = []
+bounces_leftwall_y=0
+bounces_rightwall_y=0
+temp_y=0
+temp_y2=0
+temp_y3=0
+temp_x=0
+temp_x2=0
+temp_x3=0
+ball_served_random=random.randrange(1,3)
+class MLPlay:
+    def __init__(self, side):
+        """
+        Constructor
+
+        @param side A string "1P" or "2P" indicates that the `MLPlay` is used by
+               which side.
+        """
+        self.ball_served = False    #未發球
+        self.side = side
+
+    def update(self, scene_info):
+     global bounces_leftwall_y,bounces_rightwall_y,temp_x,temp_y,temp_x2,temp_y2,temp_x3,temp_y3
+     ball_down = False    
+     while True:
+        if scene_info["status"] != "GAME_ALIVE":    #比出勝負
+            return "RESET"  #遊戲重製
+        if not self.ball_served:    #如果未發球
+            self.ball_served = True
+            print("ball_pos:",scene_info["ball"])
+            if(ball_served_random==1):
+                return "SERVE_TO_RIGHT"  #往右發球
+            else:
+                return "SERVE_TO_LEFT"  #往左發球
+        else:
+            ball_position_history.append(scene_info["ball"])    #取得球的(X,Y)    
+            if(len(ball_position_history)>1):
+                if (ball_position_history[-1][1] - ball_position_history[-2][1]) > 0:   #最後一次球的Y座標-倒數第二次的球Y座標
+                    ball_down = True
+                else:
+                    ball_down = False
+                m = 0
+
+                if (ball_position_history[-1][-2] - ball_position_history[-2][-2]) != 0:  #如果球在移動，求斜率
+                    m = ((ball_position_history[-1][-1]-ball_position_history[-2][-1]) / (ball_position_history[-1][-2]-ball_position_history[-2][-2]))
+                    #print('m=',m)
+                #(91,408)->(84,401) (84,401)->(77,394) (0,317) (195,121)
+                if(ball_down != True and m>0):
+                    if(ball_position_history[-1][-1]>275): #如果球在Y=0~200這裡反彈，下一次反彈會超過板子，不用再計算球的落點位置，只計算Y=200~500區間的彈跳
+                        temp_y=ball_position_history[-1][-1]-abs((ball_position_history[-1][-2]/scene_info["ball_speed"][-2])*scene_info["ball_speed"][-1]) #第一次反彈的Y座標
+                        temp_x=0    #第一次反彈的X座標
+                        temp_y2=temp_y-abs((195/scene_info["ball_speed"][-2])*scene_info["ball_speed"][-1]) #第二次反彈的Y座標
+                        temp_x2=195 #第二次反彈的X座標
+                        temp_x3=abs(temp_x2-abs(((temp_y2-80)/scene_info["ball_speed"][-1])*scene_info["ball_speed"][-2]))    #球最後的落點X座標
+                        temp_y3=80  #球最後的落點Y座標,因為P2板子Y座標在50，板子高度30
+                        
+                if(ball_down != True and m<0):    #同上,m=-1的情況
+                    if(ball_position_history[-1][-1]>275):
+                        temp_y=ball_position_history[-1][-1]-abs((195-ball_position_history[-1][-2])/scene_info["ball_speed"][-2]*scene_info["ball_speed"][-1])
+                        temp_x=195
+                        temp_y2=temp_y-abs((195/scene_info["ball_speed"][-2])*scene_info["ball_speed"][-1])
+                        temp_x2=0   
+                        temp_x3=abs(temp_x2-abs(((temp_y2-80)/scene_info["ball_speed"][-1])*scene_info["ball_speed"][-2]))
+                        temp_y3=80  
+                    
+                #print(temp_x,temp_y,"        ",temp_x2,temp_y2,"        ",temp_x3,temp_y3,"        ",ball_position_history[-1][-2],ball_position_history[-1][-1])
+                
+                if(ball_down ==True):     #擊球後，板子回到中心             
+                    if(scene_info["platform_2P"][-2]+20<100):
+                        return "MOVE_RIGHT" #板子右移
+                    if(scene_info["platform_2P"][-2]+20>100):
+                        return "MOVE_LEFT" #板子左移
+                    if(scene_info["platform_2P"][-2]+20==100):
+                        return "NONE"
+                else:
+                    if(scene_info["platform_2P"][-2]+20<temp_x3):   #板子中心<球最後落點
+                        return "MOVE_RIGHT" #板子右移
+                    if(scene_info["platform_2P"][-2]+20>temp_x3):#板子中心>球最後落點
+                        return "MOVE_LEFT" #板子左移
+                    else:
+                        return "NONE"
+            return "NONE"
+     
+    def reset(self):
+        """
+        Reset the status
+        """
+        self.ball_served = False
+        ball_served_random=random.randrange(1,3)
---
+++ final/KNN_TEST.py
@@ -0,0 +1,118 @@
+import pickle
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+import os
+     
+path='C:\\Users\\aiolb\\Desktop\\MLGame-beta8.0.1\\games\\pingpong\\log'
+Frame = []
+BallPosition = []
+BallSpeed=[]
+BallSpeed_x=[]
+BallSpeed_y=[]
+PlatformPosition1P = []
+PlatformPosition2P = []
+PlatformPosition1P_x = []
+BallPosition_x=[]
+BallPosition_y=[]
+files = listdir(path)
+k=0
+
+for f in files:         ##將路徑底下的檔名與路徑結合
+    allpath = join(path, f)  
+    if isfile(allpath):
+        with open(allpath , "rb") as f1:
+            data_list1 = pickle.load(f1)     
+            for ml_name in data_list1.keys():
+                if ml_name == "record_format_version":
+                    continue
+        target_record = data_list1[ml_name]
+        for n in range(0,len(target_record["scene_info"])):
+            Frame.append(target_record["scene_info"][n]["frame"])
+            PlatformPosition1P.append(target_record["scene_info"][n]["platform_1P"])
+            PlatformPosition2P.append(target_record["scene_info"][n]["platform_2P"])
+            BallPosition.append(target_record["scene_info"][n]["ball"])
+            BallSpeed.append(target_record["scene_info"][n]["ball_speed"])
+            BallPosition_x.append(target_record["scene_info"][n]["ball"][0])
+            BallPosition_y.append(target_record["scene_info"][n]["ball"][1])   
+    
+PlatX = np.array(PlatformPosition1P) [:,0][:,np.newaxis] #[:,0]->取所有第一陣列的第一個數值(X座標) #[:,np.newaxis]->陣列變成直的
+PlatX_next = PlatX[1:,:] #除了第一個值以外都要
+instrust = (PlatX_next-PlatX[0:len(PlatX_next)])/5 #板子位移量為5
+
+Ballarray = np.array(BallPosition[:-1]) 
+
+# =============================================================================
+# BallX_position = np.array(BallPosition)[:,0][:,np.newaxis]
+# BallX_position_next = BallX_position[1:,:]
+# Ball_Vx = BallX_position_next - BallX_position[0:len(BallX_position_next),0][:,np.newaxis] #球X座標的位移量
+# =============================================================================
+Ball_Vx=np.array(BallSpeed)[:-1,0][:,np.newaxis]
+# =============================================================================
+# BallY_position = np.array(BallPosition)[:,1][:,np.newaxis]
+# BallY_position_next = BallY_position[1:,:]
+# Ball_Vy = BallY_position_next - BallY_position[0:len(BallY_position_next),0][:,np.newaxis]#球Y座標的位移量
+# =============================================================================
+Ball_Vy=np.array(BallSpeed)[:-1,1][:,np.newaxis]
+x = np.hstack((Ballarray,PlatX[:-1,0][:,np.newaxis],Ball_Vx,Ball_Vy))   #[195     415     186         -9            -7          ] 
+                                                                        #[ball_x  ball_y  platform_x  ball_x_speed  ball_y_speed]
+
+np.set_printoptions(threshold=np.inf)
+y = instrust 
+y = np.array(y, dtype=int)
+
+#data=open("C:\\Users\\aiolb\\Desktop\\data.txt",'w+') 
+#print(y,file=data)
+#data.close()
+
+
+# =============================================================================
+# import csv
+# 
+# # 開啟輸出的 CSV 檔案
+# with open('C:\\Users\\aiolb\\Desktop\\data.csv', 'w', newline='') as csvFile:
+#   # 建立 CSV 檔寫入器
+#   writer = csv.writer(csvFile)
+# 
+#   # 1.直接寫出-標題
+#   writer.writerow(['球座標X','球座標Y','球速X','球速Y','板子X','預測'])
+#   for n in range(0,len(y)):
+#       writer.writerow([Ballarray[n][-2],Ballarray[n][-1],Ball_Vx[n],Ball_Vy[n],PlatX[:-1,0][:,np.newaxis][n],y[n]])
+# 
+# fig, ax = plt.subplots()
+# #ax.scatter(BallPosition_x,instrust)
+# ax.scatter(BallPosition_x,BallPosition_y)
+# plt.show()
+# =============================================================================
+np.set_printoptions(threshold=np.inf)
+#--------------------------- train & test data
+from sklearn.model_selection import train_test_split
+
+#--------------------------- train model
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score ,mean_squared_error
+from sklearn.feature_selection import SelectKBest, f_regression,GenericUnivariateSelect
+
+
+# =============================================================================
+# selector = GenericUnivariateSelect(f_regression, mode='fpr', param=0.0000001)
+# 
+# x_new = selector.fit_transform(x, y)
+# print(x_new.shape)
+# print(selector.get_support())
+# =============================================================================
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state = 1)
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(x_train,y_train)
+
+yknn_bef_scaler = knn.predict(x_test)
+r2 = accuracy_score(yknn_bef_scaler,y_test)
+mse = mean_squared_error(y_test, yknn_bef_scaler)
+
+filename = "C:\\Users\\aiolb\\Desktop\\MLGame-beta8.0.1\\games\\pingpong\\ml\\1P.sav"
+pickle.dump(knn,open(filename,"wb"))
+print("R2:",r2) #R2越趨近1越好
+print("MSE:",mse) #MSE越小越好
